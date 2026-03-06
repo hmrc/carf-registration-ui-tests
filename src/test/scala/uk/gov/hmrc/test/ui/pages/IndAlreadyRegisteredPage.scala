928cc98aee657ba6c1fcdf558cc74bb9813d89e5
Prashant Soni <180386844+Prashant-digital@users.noreply.github.com>
2026-03-06 12:04:11 +0000

    Added script for CARF-261 (#56)
    * Added script for CARF-261
    
    * CARF-261 moved test from existing problem page spec

---
+++ src/test/scala/uk/gov/hmrc/test/ui/pages/IndAlreadyRegisteredPage.scala
@@ -0,0 +1,7 @@
+package uk.gov.hmrc.test.ui.pages
+
+object IndAlreadyRegisteredPage extends BasePage {
+
+  override val pageUrl: String =
+    baseUrl + "/register/problem/individual-already-registered"
+}
